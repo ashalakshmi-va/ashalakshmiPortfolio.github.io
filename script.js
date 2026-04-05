1ca05c8ee5a644203ac4bdf87ef7f298504b9b4d
ashalakshmi-va <ashalakshmi.anirudhan@gmail.com>
2026-04-05 11:20:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// DARK MODE SAVE
 function toggleDarkMode() {
   document.body.classList.toggle("dark");
+  localStorage.setItem("theme", document.body.classList.contains("dark"));
 }
+
+// LOAD THEME
+if (localStorage.getItem("theme") === "true") {
+  document.body.classList.add("dark");
+}
+
+// SCROLL ANIMATION
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
